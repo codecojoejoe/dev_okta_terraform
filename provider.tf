83ea38f4e6b90a8eb9689c1bef10d371720dde55
Joe Willis <58248380+codecojoejoe@users.noreply.github.com>
2023-09-04 23:14:37 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,12 @@
-Okta Provider
+terraform {
+  required_providers {
+    okta = {
+      source = "okta/okta"
+      version = "4.3.0"
+    }
+  }
+}
+
+provider "okta" {
+  # Configuration options
+}
