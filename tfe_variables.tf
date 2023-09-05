7ff8f87e33e20203dd8bd22f24e47c600ca8bd13
Joe Willis <joedwillis@gmail.com>
2023-09-05 15:06:44 -0500

    Commit

--- tfe_variables.tf
+++ tfe_variables.tf
@@ -40,13 +40,3 @@ resource "tfe_variable" "dev_okta_terraform_brand_name" {
   key          = "brand_name"
   value        = "Included Dev Sandbox"
 }
-
-  resource "tfe_workspace" "dev_okta_terraform" {
-  name               = "dev-okta-terraform"
-  organization       = var.org_name
-  description        = "Workspace to build the Included Dev Okta org via infrastructure-as-code"
-  allow_destroy_plan = false
-  terraform_version  = "1.5.6"
-}
-
-
