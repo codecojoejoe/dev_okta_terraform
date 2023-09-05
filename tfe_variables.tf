535acb58c1945bb91e35d9315d57af357acc948b
Joe Willis <58248380+codecojoejoe@users.noreply.github.com>
2023-09-04 23:43:47 -0500

    Update tfe_variables.tf

--- tfe_variables.tf
+++ tfe_variables.tf
@@ -1,50 +1,50 @@
 # ------------------------------------------------------------------------------------------
 # Okta sandbox
 
-resource "tfe_variable" "okta_sandbox_terraform_org_name" {
+resource "tfe_variable" "Dev_Okta_Terraform_org_name" {
   category     = "terraform"
   description  = "Okta org sub-domain"
-  workspace_id = tfe_workspace.okta_sandbox_terraform.id
+  workspace_id = tfe_workspace.Dev_Okta_Terraform.id
   hcl          = false
   sensitive    = false
   key          = "org_name"
   value        = "included-dev"
 }
 
-resource "tfe_variable" "okta_sandbox_terraform_base_url" {
+resource "tfe_variable" "Dev_Okta_Terraform_base_url" {
   category     = "terraform"
   description  = "Okta org domain"
-  workspace_id = tfe_workspace.okta_sandbox_terraform.id
+  workspace_id = tfe_workspace.Dev_Okta_Terraform.id
   hcl          = false
   sensitive    = false
   key          = "base_url"
   value        = "oktapreview.com"
 }
 
-resource "tfe_variable" "okta_sandbox_terraform_domain" {
+resource "tfe_variable" "Dev_Okta_Terraform_domain" {
   category     = "terraform"
   description  = "Default email address domain for all users in this Okta org"
-  workspace_id = tfe_workspace.okta_sandbox_terraform.id
+  workspace_id = tfe_workspace.Dev_Okta_Terraform.id
   hcl          = false
   sensitive    = false
   key          = "domain"
   value        = "includedhealth.com"
 }
 
-resource "tfe_variable" "okta_sandbox_terraform_brand_name" {
+resource "tfe_variable" "Dev_Okta_Terraform_brand_name" {
   category     = "terraform"
   description  = "Full company brand name used for this Okta org"
-  workspace_id = tfe_workspace.okta_sandbox_terraform.id
+  workspace_id = tfe_workspace.Dev_Okta_Terraform.id
   hcl          = false
   sensitive    = false
   key          = "brand_name"
   value        = "Included Health Sandbox"
 }
 
-resource "tfe_variable" "okta_sandbox_terraform_human_okta_admin_users" {
+resource "tfe_variable" "Dev_Okta_Terraform_human_okta_admin_users" {
   category     = "terraform"
   description  = "Map of Okta user IDs for human Okta admin users in this Okta org"
-  workspace_id = tfe_workspace.okta_sandbox_terraform.id
+  workspace_id = tfe_workspace.Dev_Okta_Terraform.id
   hcl          = true
   sensitive    = false
   key          = "human_okta_admin_users"
@@ -55,10 +55,10 @@ resource "tfe_variable" "okta_sandbox_terraform_human_okta_admin_users" {
 EOT
 }
 
-resource "tfe_variable" "okta_sandbox_terraform_applications" {
+resource "tfe_variable" "Dev_Okta_Terraform_applications" {
   category     = "terraform"
   description  = "Map of Okta application IDs for applications in this Okta org"
-  workspace_id = tfe_workspace.okta_sandbox_terraform.id
+  workspace_id = tfe_workspace.Dev_Okta_Terraform.id
   hcl          = true
   sensitive    = false
   key          = "applications"
@@ -71,7 +71,7 @@ EOT
 resource "tfe_variable" "okta_sandbox_applications_deny_all_unauthorized_users" {
   category     = "terraform"
   description  = "List of applications that should have the deny_all_unauthorized_users signon policy"
-  workspace_id = tfe_workspace.okta_sandbox_terraform.id
+  workspace_id = tfe_workspace.Dev_Okta_Terraform.id
   hcl          = true
   sensitive    = false
   key          = "applications_deny_all_unauthorized_users"
@@ -91,7 +91,7 @@ EOT
 resource "tfe_variable" "okta_sandbox_applications_deny_unmanaged_windows_clients" {
   category     = "terraform"
   description  = "List of applications that should be denied to unmanaged windows clients"
-  workspace_id = tfe_workspace.okta_sandbox_terraform.id
+  workspace_id = tfe_workspace.Dev_Okta_Terraform.id
   hcl          = true
   sensitive    = false
   key          = "applications_deny_unmanaged_windows_clients"
@@ -105,7 +105,7 @@ EOT
 resource "tfe_variable" "okta_applications_deny_external_network_zones" {
   category     = "terraform"
   description  = "List of applications that should have the deny_external_network_zones policy"
-  workspace_id = tfe_workspace.okta_sandbox_terraform.id
+  workspace_id = tfe_workspace.Dev_Okta_Terraform.id
   hcl          = true
   sensitive    = false
   key          = "applications_deny_external_network_zones"
@@ -119,7 +119,7 @@ EOT
 resource "tfe_variable" "okta_sandbox_applications_require_mfa_every_sign_on" {
   category     = "terraform"
   description  = "List of applications that should have the require_mfa_every_sign_on signon policy"
-  workspace_id = tfe_workspace.okta_sandbox_terraform.id
+  workspace_id = tfe_workspace.Dev_Okta_Terraform.id
   hcl          = true
   sensitive    = false
   key          = "applications_require_mfa_every_sign_on"
