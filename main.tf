987a7d2c889f734e67a7c70b270bc5a91b6b7e58
Joe Willis <58248380+codecojoejoe@users.noreply.github.com>
2023-09-04 22:58:24 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    okta = {
+      source = "okta/okta"
+            version = "4.3.0"
+    }
+  }
+}
+
+provider "okta" {
+  org_name    = "https://included-dev"
+  base_url    = "oktapreview.com"
+  client_id   = "0oa9uf3rpuVVGrtBc1d7"
+  scopes      = ["okta.groups.manage"]
+  private_key = "/Users/joe.willis/DevOktaTerraform/okta-tf-ihdev-privatekey"
+}
+
+resource "okta_group" "example_group" {
+  name = "Example Group"
+}
