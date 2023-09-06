ab25df7343b1203f1b5a2d609069b4490c30dd76
Joe Willis <joedwillis@gmail.com>
2023-09-05 21:53:10 -0500

    Update tfe_workspaces.tf

--- tfe_workspaces.tf
+++ tfe_workspaces.tf
@@ -2,7 +2,7 @@
 # Okta sandbox
 
 resource "tfe_workspace" "dev_okta_terraform" {
-  name               = "dev_okta_terraform"
+  name               = "demo-repository"
   organization       = var.org_name
   description        = "Workspace to build the Included Dev Okta org via infrastructure-as-code"
   allow_destroy_plan = false
