306f161822413b1d54e69faf73eb241242814c70
Joe Willis <58248380+codecojoejoe@users.noreply.github.com>
2023-12-04 00:42:59 -0600

    2023-12-04T06:42:52.067Z - Automated by Provider - Main

--- main.tf
+++ main.tf
@@ -1,45 +1,4 @@
 ﻿terraform {
-  required_providers {
-    okta = {
-      source  = "okta/okta"
-      version = "3.37.0"
-    }
-  }
-  backend "remote" {
-    organization = "included-dev"
-
-
-    workspaces {
-      name = "dev-terraform"
-    }
-  }
-}
-
-
-provider "okta" {
-  org_name  = var.org_name
-  base_url  = var.base_url
-  api_token = var.api_token
-}
-terraform {
- required_providers {
- okta = {
- source = "okta/okta"
- version = ""
- }
- }
- backend "remote" {
- organization = "included-dev"
- workspaces {
- name = "dev-terraform"
- }
- }
-}
-provider "okta" {
- org_name = var.org_name
- base_url = var.base_url
- api_token = var.api_token
-}terraform {
  required_providers {
  okta = {
  source = "okta/okta"
