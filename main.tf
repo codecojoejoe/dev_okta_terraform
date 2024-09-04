25f192729f243b51b725e749b48503fcefbd3127
Joe Willis <58248380+codecojoejoe@users.noreply.github.com>
2024-09-04 11:00:52 -0500

    2024-09-04T16:00:27.677Z - Automated by Provider - Main

--- main.tf
+++ main.tf
@@ -1,1 +1,19 @@
-
+﻿terraform {
+ required_providers {
+ okta = {
+ source = "okta/okta"
+ version = "4.10.0"
+ }
+ }
+ backend "remote" {
+ organization = "included-dev"
+ workspaces {
+ name = "dev-terraform"
+ }
+ }
+}
+provider "okta" {
+ org_name = var.org_name
+ base_url = var.base_url
+ api_token = var.api_token
+}
